988adc487e6aecda55f449ad4e16707c8f344d93
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-14 18:13:10 +0300

    publish

---
+++ publish/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    $('#editWorker').on('click', '.editRow', function () {
+        var id = parseInt($(this).find('.rowId').html());
+        document.location.href = '/Worker/Edit?id=' + id;
+    });
+});
+
+$(function () {
+    $('#editCompany').on('click', '.editRow', function () {
+        var id = parseInt($(this).find('.rowId').html());
+        document.location.href = '/Company/Edit?id=' + id;
+    });
+});
+
+$(function () {
+    $('#editPosition').on('click', '.editRow', function () {
+        var id = parseInt($(this).find('.rowId').html());
+        document.location.href = '/Position/Edit?id=' + id;
+    });
+});
+$(function () {
+    $('#editFormType').on('click', '.editRow', function () {
+        var id = parseInt($(this).find('.rowId').html());
+        document.location.href = '/FormType/Edit?id=' + id;
+    });
+});
+
+
+function fillInputs(form) {
+    let url = new URL(window.location.href);
+    if (!url.search) return;
+    for (let [name, value] of url.searchParams) {
+        form.elements[name].value = value;
+    }
+}
+fillInputs(document.forms.find);
